bc74befd7f4ae704b5af4b876f50fd4129b422fc
Souza Victor <102697200+VictorSouza02@users.noreply.github.com>
2022-06-07 12:38:32 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Get the values */
+
+const Name = document.querySelector(".name")
+const subject = prompt('Digite o nome do aluno:')
+
+Name.textContent = subject
+
+const score = document.querySelector(".value")
+const number = prompt('Digite a nota do aluno:')
+
+score.textContent = number
+console.log(number)
+    
+/* Logic */
+let situation = document.querySelector(".situation")
+
+if (number >= 6){
+    situation.textContent = 'Aprovado'
+    situation.style.color = 'rgb(19, 204, 19)'
+} 
+else{
+    situation.textContent = 'Reprovado'
+    situation.style.color = 'rgb(223, 14, 14)'
+}
