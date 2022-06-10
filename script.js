e986cdd892ebe9feec9dfe26107da80e76451ec1
Víctor Souza <102697200+VictorSouza02@users.noreply.github.com>
2022-06-10 06:46:42 -0300

    window.onload added

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-/* Get the values */
+window.onload = function(){
+    /* Get the values */
+    const Name = document.querySelector(".name")
+    const subject = prompt('Digite o nome do aluno:')
+    Name.textContent = subject
 
-const Name = document.querySelector(".name")
-const subject = prompt('Digite o nome do aluno:')
-
-Name.textContent = subject
-
-const score = document.querySelector(".value")
-const number = prompt('Digite a nota do aluno:')
-
-score.textContent = number
+    const score = document.querySelector(".value")
+    const number = prompt('Digite a nota do aluno:')
+    score.textContent = number
+        
+    /* Logic */
+    let situation = document.querySelector(".situation")
     
-/* Logic */
-let situation = document.querySelector(".situation")
-
-if (number >= 6){
-    situation.textContent = 'Aprovado'
-    situation.style.color = 'rgb(19, 204, 19)'
-} 
-else{
-    situation.textContent = 'Reprovado'
-    situation.style.color = 'rgb(223, 14, 14)'
+    if (number >= 6){
+        situation.textContent = 'Aprovado'
+        situation.style.color = 'rgb(19, 204, 19)'
+    } else{
+        situation.textContent = 'Reprovado'
+        situation.style.color = 'rgb(223, 14, 14)'
+    }
 }
